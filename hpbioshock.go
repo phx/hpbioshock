ae09b1fb135ed7bb781e92c1779fe2f0da73a469
phx <oldjamey@gmail.com>
2020-02-26 15:02:06 -0800

    added new windows commands

--- hpbioshock.go
+++ hpbioshock.go
@@ -1,3 +1,6 @@
+// HP BCU versions lower than 3.0.3.1 are required to check plain-text passwords.
+//'go run hpbioshock.go' must be run from an elevated command prompt.
+
 package main
 
 import (
@@ -6,30 +9,30 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
-
-func errcheck(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 
 func main() {
 	file, err := os.Open("brutalist.txt")
-	errcheck(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer file.Close()
-
+	bcu := "C:\\PROGRA~2\\Hewlett-Packard\\BIOS Configuration Utility\\BiosConfigUtility64.exe"
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		pass := scanner.Text()
-		log.Println("Testing " + pass + "...")
-		out, err := exec.Command("echo", pass).CombinedOutput()
-		errcheck(err)
-		fmt.Println(string(out))
+		log.Println("Trying " + pass + "...")
+		out, _ := exec.Command(bcu, "cspswd:\"BIOS password\"", "nspswd:\"\"").CombinedOutput()
+		res := string(out)
+		if !strings.Contains(res, "invalid") {
+			fmt.Println(pass)
+			fmt.Println(res)
+			fmt.Println("*** SUCCESS ***")
+			return
+		}
 	}
-
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
 }
